08e8461043bfa92eb7d0c6692417b9ed5811610c
front-ed <edwinschutjes@gmail.com>
2023-03-27 20:45:53 +0200

    add get person from API and data destructuring

--- script.js
+++ script.js
@@ -1,14 +1,55 @@
 /* ---- TARGETS && INITIALIZERS ---- */
-/* ---- SCRIPT ---- */
+
+import getElement from "./getElement.js";
+
+const url = "https://randomuser.me/api/";
+
+const userImg = getElement(".user-img");
+const userTitle = getElement(".user-title");
+const userValue = getElement(".user-value");
+const randomBtn = getElement(".btn");
+// Get all icons nodelist and spread to array
+const icons = [...document.querySelectorAll(".icon")];
+
 /* ---- FUNCTIONS ---- */
 
-console.log("hello world");
+// Get random user from API
+const getUser = async () => {
+  const response = await fetch(url);
+  // console.log(response);
+  const data = await response.json();
+  // console.log(data);
 
-//-- Get DOM element function
-function getElement(selector) {
-  const element = document.querySelector(selector);
-  if (element) {
-    return element;
-  }
-  throw new Error(`${element} is not found`);
-}
+  // destructure the incoming object
+  const person = data.results[0];
+  const { phone, email } = person;
+  const { large: img } = person.picture;
+  const { password } = person.login;
+  const { first, last } = person.name;
+  const { age } = person.dob;
+  const {
+    street: { number, name },
+  } = person.location;
+  // Return a new object with destructerd values
+  return {
+    phone,
+    email,
+    img,
+    password,
+    age,
+    street: `${number} ${name}`,
+    name: `${first} ${last}`,
+  };
+};
+
+// Show user function
+const showUser = async () => {
+  // await the async promise from getUser
+  const newPerson = await getUser();
+  // display user in DOM
+};
+
+// ON Page Load: trigger show user
+window.addEventListener("DOMContentLoaded", showUser);
+// ON Button Click: trigger show user
+randomBtn.addEventListener("click", showUser);
